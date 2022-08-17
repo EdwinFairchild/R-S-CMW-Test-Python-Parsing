55a79b58251007ac72393125f43f357a125d3724
Eddie <eddie@edwinfairchild.com>
2022-08-16 22:05:18 -0500

    clean up

--- jsonTestParse.py
+++ jsonTestParse.py
@@ -2,15 +2,11 @@ import json
 from matplotlib import pyplot as plt
 import numpy as np
 import sys
-# Opening JSON file
+
 testresultsFile = str(sys.argv[1])
 imageFileName = str(sys.argv[2])
-
-
 f = open(testresultsFile)
-  
-# returns JSON object as 
-# a dictionary
+
 data = json.load(f)
 xRaw = data['SubElements'][8]['TestCases'][0]['Elements'][1]['Measured'][0]['Values'][0]['Value']
 xRaw = xRaw.split(',')
@@ -32,12 +28,9 @@ for val in yRaw:
    
 yFixed = yFixed[::-1]
 
-
 plt.rcParams["figure.figsize"] = [7.00, 3.50]
 plt.rcParams["figure.autolayout"] = True
-
 
 default_x_ticks = range(len(xFixed)) 
 plt.plot(xFixed, yFixed)
 plt.savefig(imageFileName, bbox_inches='tight')
-
--- texttestParse.py
+++ texttestParse.py
@@ -10,16 +10,12 @@ data = file.readlines()
 xRaw = np.array(data[0].split(",")[::-1])
 yRaw = np.array(data[1].split(",")[::-1])
 
-
-
 xFixed= []
 for val in xRaw:
     if val != '':
         xFixed.append(float(val)) 
 
 xFixed = xFixed[::-1]
-
-
 
 yFixed= []
 for val in yRaw:
@@ -31,8 +27,6 @@ yFixed = yFixed[::-1]
 plt.rcParams["figure.figsize"] = [7.00, 3.50]
 plt.rcParams["figure.autolayout"] = True
 
-
 default_x_ticks = range(len(xFixed)) 
 plt.plot(xFixed, yFixed)
 plt.savefig(imageFileName, bbox_inches='tight')
-
